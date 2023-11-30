71d6a2bdac68281c53344cb5bc4c67ff9d9495a3
TidalCub <Leon.skinner@hotmail.co.uk>
2023-11-30 21:07:07 +0000

    add product controler and modal

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products, controller: 'product', only: [:index, :new, :create]
   resources :manage, only: [:index]
   resources :categories, controller: 'category', only: [:index, :new, :create]
 end
