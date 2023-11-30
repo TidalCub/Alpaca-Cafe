efc81e084687e32ed5c2b7e5868eacefa2db1ae8
TidalCub <Leon.skinner@hotmail.co.uk>
2023-11-30 22:07:22 +0000

    show products with catagories on menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'menu/show'
   resources :products, controller: 'product', only: [:index, :new, :create]
   resources :manage, only: [:index]
   resources :categories, controller: 'category', only: [:index, :new, :create]
