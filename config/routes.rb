2e45ef350e6f6c0acc3e63f22ba865a02e3001c3
TidalCub <Leon.skinner@hotmail.co.uk>
2023-11-30 22:26:42 +0000

    add header

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'menu/show'
+  root 'category#index'
   resources :products, controller: 'product', only: [:index, :new, :create]
   resources :manage, only: [:index]
   resources :categories, controller: 'category', only: [:index, :new, :create]
