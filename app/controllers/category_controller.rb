81c1c97c4673bc60f1d0e4f56427363f79f14ed1
Leon Skinner <Leon.skinner@bt.com>
2023-11-30 15:52:55 +0000

    add form to add categories

---
+++ app/controllers/category_controller.rb
@@ -0,0 +1,25 @@
+class CategoryController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      
+      redirect_to categories_path, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.permit(:name)
+  end
+end
