31ff887a95532801f19c91f466272672136ccab8
Leon Skinner <Leon.skinner@bt.com>
2023-11-30 14:09:34 +0000

    creat tables

---
+++ db/migrate/20231130135414_create_tables.rb
@@ -0,0 +1,43 @@
+class CreateTables < ActiveRecord::Migration[7.0]
+  def change
+    create_table :statuses do |t|
+      t.string :name
+      t.timestamps
+    end
+
+    create_table :categories do |t|
+      t.string :name
+      t.timestamps
+    end
+
+    create_table :users do |t|
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.date :date_of_birth
+      t.string :password
+      t.timestamps
+    end
+
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.decimal :price, precision: 8, scale: 2
+      t.references :category, foreign_key: true
+      t.timestamps
+    end
+
+    create_table :orders do |t|
+      t.references :user, foreign_key: true
+      t.references :status, foreign_key: true
+      t.timestamps
+    end
+
+    create_table :order_items do |t|
+      t.references :order, foreign_key: true
+      t.references :product, foreign_key: true
+      t.integer :quantity
+      t.timestamps
+    end
+  end
+end
