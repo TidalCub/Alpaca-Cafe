a0bd060a4bf77920140db7f34e23cef26ba31651
Leon Skinner <Leon.skinner@bt.com>
2023-12-05 13:11:28 +0000

    add login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'categories#index'
+  devise_for :users
+  root 'category#index'
   resources :products, controller: 'product', only: [:index, :new, :create]
   resources :manage, only: [:index]
   resources :categories, controller: 'category', only: [:index, :new, :create]
