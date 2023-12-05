35c07092cf43faf83731b1dc19a473914af73246
Leon Skinner <Leon.skinner@bt.com>
2023-12-05 14:24:06 +0000

    add relations to modals

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,5 @@
+class OrdersController < ApplicationController
+  def add_to_order
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :products, controller: 'product', only: [:index, :new, :create]
   resources :manage, only: [:index]
   resources :categories, controller: 'category', only: [:index, :new, :create]
+  resources :orders
 end
