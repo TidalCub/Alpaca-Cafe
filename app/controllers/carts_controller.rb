0091a2e8dde382f1850026f23eff05fdc4fb6bb7
TidalCub <Leon.skinner@hotmail.co.uk>
2023-12-08 13:20:21 +0000

    remove cart controller

--- app/controllers/carts_controller.rb
+++
@@ -1,9 +0,0 @@
-class CartsController < ApplicationController
-  def index
-    unless current_user
-      redirect_to user_session_path, alert: "you need to be signed in to view your cart"
-      return
-    end
-    redirect_to order_url(get_user_basket)
-  end
-end
