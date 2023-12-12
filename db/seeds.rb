050e348e89c196857beb78810db74649708196a7
Leon Skinner <Leon.skinner@bt.com>
2023-12-12 15:22:49 +0000

    show ingredient modifers on cart and checkout

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+ProductModifyer.destroy_all
 OrderItem.destroy_all
 Order.destroy_all
 Recipe.destroy_all
