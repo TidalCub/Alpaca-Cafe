1d908ca51dce94856fb561d2dedf62a5fd30c38b
Leon Skinner <Leon.skinner@bt.com>
2024-11-14 14:30:36 +0000

    refactor product controller

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,5 +1,6 @@
 class ProductController < ApplicationController
   skip_before_action :verify_authenticity_token
+
   def index
     @products = Product.all
   end
@@ -18,9 +19,8 @@ class ProductController < ApplicationController
   end
 
   def add_to_basket
-    basket = current_user.basket
     product = Product.find(add_to_basket_params[:product])
-    order_item = OrderItem.create(order: basket, product_id: product.id)
+    order_item = current_user.basket.order_items.create(product_id: product.id)
     product_modifyer(params, order_item)
     redirect_back(fallback_location: categories_path)
   end
@@ -28,10 +28,9 @@ class ProductController < ApplicationController
   private
 
   def product_modifyer(params, order_item)
-    IngredientGroup.all.each do |ingredient_group|
+    IngredientGroup.all.find_each do |ingredient_group|
       if params[ingredient_group.name].present? && !Ingredient.find(params[ingredient_group.name]).is_default
-        ProductModifyer.create(order_item_id: order_item.id,
-                               ingredient_id: Ingredient.find(params[ingredient_group.name]).id)
+        order_item.product_modifyers.create(ingredient_id: Ingredient.find(params[ingredient_group.name]).id)
       end
     end
   end
