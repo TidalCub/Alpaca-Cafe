bd1773ae31d55c35e058e3c0fd2dbd0cb1e11bc7
Leon Skinner <Leon.skinner@bt.com>
2024-11-14 14:41:35 +0000

    update to rails 7.2.2

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
 
-  enum(state: { pending: 0, paid: 1, completed: 2 })
+  enum :state, { pending: 0, paid: 1, completed: 2 }
 
   def total
     order_items.sum { |item| item.product.price }
