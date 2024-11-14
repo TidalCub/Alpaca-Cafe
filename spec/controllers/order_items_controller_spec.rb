9ed3071cc4344d30302d573eda4e85564e859995
Leon Skinner <Leon.skinner@bt.com>
2024-11-14 14:09:18 +0000

    add test for order items

---
+++ spec/controllers/order_items_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe OrderItemsController, type: :controller do
+  describe 'DELETE #destroy' do
+    subject { delete :destroy, params: { id: order_item.id } }
+    let(:order_item) { create(:order_item) }
+
+    it 'deletes the order item' do
+      subject
+      expect(OrderItem.exists?(order_item.id)).to be_falsey
+    end
+  end
+end
