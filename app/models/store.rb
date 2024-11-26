5de0ca98d5e6583222ecf0733441be7dcd2cbb3f
Leon Skinner <Leon.skinner@bt.com>
2024-11-26 16:07:04 +0000

    add relations

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,4 +3,6 @@ class Store < ApplicationRecord
   has_many :orders
   has_many :menus
   has_many :products, through: :menus
+  has_many :ingredient_stocks
+  has_many :ingredients, through: :ingredient_stocks
 end
