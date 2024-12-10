88bd93b2925bb5692c4143539e2b8154a1932ec2
Leon Skinner <Leon.skinner@bt.com>
2024-12-10 16:14:14 +0000

    rotate stripe test key and filter it from fixtures

--- config/initializers/vcr.rb
+++ config/initializers/vcr.rb
@@ -3,4 +3,5 @@
 VCR.configure do |vcr_config|
   vcr_config.cassette_library_dir = 'fixtures/vcr_cassettes'
   vcr_config.hook_into :webmock
+  vcr_config.filter_sensitive_data('<stripe_secret>') { Rails.application.credentials.dig(:development, :stripe, :secret_key) }
 end
