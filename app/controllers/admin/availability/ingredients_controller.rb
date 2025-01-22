5387f282898e057b712c10bfc10467b9209df871
Leon Skinner <Leon.skinner@bt.com>
2025-01-22 15:45:30 +0000

    add avalability for ingredients

---
+++ app/controllers/admin/availability/ingredients_controller.rb
@@ -0,0 +1,27 @@
+module Admin
+  module Availability
+    class IngredientsController < ApplicationController
+      before_action :set_store
+
+      def index
+        @ingredients_stock_by_group = @store.ingredient_stocks.includes(ingredient: :ingredient_group).group_by { |ingredient_stocks| ingredient_stocks.ingredient.ingredient_group }
+      end
+
+      def update
+        params[:ingredient_stocks].each do |id, ingredient_stock|
+          ingredient_stock_item = IngredientStock.find(id)
+          ingredient_stock_availability = ingredient_stock == '1'
+          ingredient_stock_item.update(available: ingredient_stock_availability)
+        end
+        store = Store.find(params[:store_id])
+        redirect_to store_name_ingredient_path(store_name: store.name), alert: 'Availability updated successfully.'
+      end
+
+      private
+
+      def set_store
+        @store = Store.find_by(name: params[:store_name])
+      end
+    end
+  end
+end
---
+++ spec/controllers/admin/availability/ingredients_controller_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Admin::Availability::IngredientsController, type: :controller do
+  let(:category) { create(:category) }
+  let(:store) { create(:store) }
+  let(:ingredient) { create(:ingredient) }
+  let!(:ingredient_stock) { create(:ingredient_stock, store: store, ingredient: ingredient) }
+  let(:ingredient_stock_group) { store.ingredient_stocks.includes(ingredient: :ingredient_group).group_by { |ingredient_stocks| ingredient_stocks.ingredient.ingredient_group } }
+
+  describe 'GET #index' do
+    subject { get :index, params: { store_name: store.name } }
+
+    it 'assigns all the ingredients availabilities to @ingredients_stock for a specific store' do
+      subject
+      expect(assigns(:ingredients_stock_by_group)).to eq(ingredient_stock_group)
+    end
+  end
+
+  describe 'PATCH #update' do
+    subject { patch :update, params: { store_name: store.name } }
+  end
+end
