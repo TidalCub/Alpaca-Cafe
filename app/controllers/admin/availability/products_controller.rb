4ddd68f8ce94fb58acae31ad1c75d35638e92a90
Leon Skinner <Leon.skinner@bt.com>
2025-01-22 15:15:15 +0000

    add the ability to change product avaliability

--- app/controllers/admin/availability/products_controller.rb
+++ app/controllers/admin/availability/products_controller.rb
@@ -1,16 +1,27 @@
-class Admin::Availability::ProductsController < ApplicationController
-  before_action :set_store
+module Admin
+  module Availability
+    class ProductsController < ApplicationController
+      before_action :set_store
 
-  def index
-    @menus_by_category = @store.menus.includes(product: :category).group_by { |menu| menu.product.category }
-  end
+      def index
+        @menus_by_category = @store.menus.includes(product: :category).group_by { |menu| menu.product.category }
+      end
 
-  def update
-  end
+      def update
+        params[:menu_items].each do |id, menu_item_available|
+          menu_item = Menu.find(id)
+          menu_item_available = menu_item_available == '1'
+          menu_item.update(available: menu_item_available)
+        end
+        store = Store.find(params[:store_id])
+        redirect_to store_name_products_path(store_name: store.name), alert: 'Availability updated successfully.'
+      end
 
-  private
+      private
 
-  def set_store
-    @store = Store.find_by(name: params[:store_name])
+      def set_store
+        @store = Store.find_by(name: params[:store_name])
+      end
+    end
   end
 end
