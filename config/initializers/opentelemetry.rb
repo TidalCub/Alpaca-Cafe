2034ce045f14a75f797eeba424ad0d6f52638b76
Leon Skinner <Leon.skinner@bt.com>
2025-03-06 18:28:51 +0000

    add open telem init

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,11 +1,8 @@
 require 'opentelemetry/sdk'
 require 'opentelemetry/instrumentation/all'
+
+ENV['OTEL_TRACES_EXPORTER'] = 'console'
 OpenTelemetry::SDK.configure do |c|
   c.service_name = 'alpaca-cafe'
   c.use_all()
-  c.add_span_processor(
-    OpenTelemetry::SDK::Trace::Export::SimpleSpanProcessor.new(
-      OpenTelemetry::Exporter::Console.new
-    )
-  )
 end
