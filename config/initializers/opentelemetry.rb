4ef6a9857ec58643b65438559ffc2a02a3d92b3d
leon <leon.skinner@hotmail.co.uk>
2025-03-20 16:59:34 +0000

    add rake task and prevent sentry and OT from running in test

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,8 +1,10 @@
 require 'opentelemetry/sdk'
 require 'opentelemetry/instrumentation/all'
 
-ENV['OTEL_TRACES_EXPORTER'] = 'console'
-OpenTelemetry::SDK.configure do |c|
-  c.service_name = 'alpaca-cafe'
-  c.use_all()
+unless Rails.env.local?
+  ENV['OTEL_TRACES_EXPORTER'] = 'console'
+  OpenTelemetry::SDK.configure do |c|
+    c.service_name = 'alpaca-cafe'
+    c.use_all()
+  end
 end
