249f2b9a8de6fa5ebccf96291d48842b3fbf0f2d
Leon Skinner <Leon.skinner@bt.com>
2025-03-25 12:11:04 +0000

    move if statment to load prometheus

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 # if ENV["PROMETHEUS_ENABLED"] == "1" && ENV["IN_PUMA_WORKER"] == "1" && !Rails.env.test?
-if Rails.env.production?
   require "prometheus_exporter/instrumentation"
   require "prometheus_exporter/middleware"
   require "prometheus_exporter/client"
 
+if Rails.env.test?
   # This reports stats per request like HTTP status and timings
   Rails.application.middleware.unshift(PrometheusExporter::Middleware)
 
