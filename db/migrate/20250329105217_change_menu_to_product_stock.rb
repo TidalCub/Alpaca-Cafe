ade2bc8469211eb9cec07c962c774165636fffd0
leon <leon.skinner@hotmail.co.uk>
2025-03-29 13:27:13 +0000

    change migration to rename primary key

--- db/migrate/20250329105217_change_menu_to_product_stock.rb
+++ db/migrate/20250329105217_change_menu_to_product_stock.rb
@@ -1,5 +1,6 @@
 class ChangeMenuToProductStock < ActiveRecord::Migration[8.0]
   def change
+    rename_index :menus, 'menus_pkey', 'product_stock_pkey' 
     rename_table :menus, :product_stocks
   end
 end
