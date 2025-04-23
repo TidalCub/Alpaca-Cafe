e5aaba3d34fc25f71e5625265ea793ebb75d222b
Leon Skinner <Leon.skinner@bt.com>
2025-04-23 15:02:17 +0100

    add stripe payment service

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,9 +10,6 @@ class Order < ApplicationRecord
 
   enum :state, { new_order: 0, pending: 1, on_checkout: 2, requires_capture: 3, processing: 4, paid: 5, payment_failed: 6, completed: 7, expired: 8 }
 
-  def total
-    order_items.sum { |item| item.product.price }
-  end
   aasm column: :state, enum: true do # rubocop:disable Metrics/BlockLength
     state :new_order, initial: true
     state :pending, :on_checkout, :paid, :completed, :expired, :payment_failed, :requires_capture, :processing
@@ -24,15 +21,7 @@ class Order < ApplicationRecord
     event :checkout do
       transitions from: %i[new_order pending on_checkout], to: :on_checkout
       after do
-        stripe_payment_intent = StripePaymentintentService.new(stripe_total_price, user, self)
-        if payment_intent.present? && client_secret.present?
-          stripe_payment_intent.update(payment_intent) if stripe_payment_intent.status != 'requires_capture'
-        else
-          payment_intent = stripe_payment_intent.create
-          self.payment_intent = payment_intent.id
-          self.client_secret = payment_intent.client_secret
-          save
-        end
+        payment_service = StripePaymentService.new(self).create_or_update
       end
     end
 
@@ -46,8 +35,7 @@ class Order < ApplicationRecord
     event :check_in do
       transitions from: :requires_capture, to: :processing
       after do
-        stripe_payment_intent = StripePaymentintentService.new(nil, user, self)
-        stripe_payment_intent.capture
+        StripePaymentService.new(self).capture_payment
         OrderMailer.payment_confirmation(self).deliver_now!
         PrintReceiptService.new(self).send
       end
@@ -73,6 +61,10 @@ class Order < ApplicationRecord
     event :failed do
       transitions from: %i[on_checkout requires_capture processing], to: :payment_failed
     end
+  end
+
+  def total
+    order_items.sum { |item| item.product.price }
   end
 
   def stripe_total_price
---
+++ app/services/stripe_payment_service.rb
@@ -0,0 +1,30 @@
+class StripePaymentService
+  def initialize(order)
+    @order = order
+    @user = order.user
+  end
+
+  def create_or_update
+    if @order.payment_intent.present? && @order.client_secret.present?
+      update_payment_intent
+    else
+      create_payment_intent
+    end
+  end
+
+  def create_payment_intent
+    stripe_payment_intent = StripePaymentintentService.new(@order.stripe_total_price, @user, @order)
+    payment_intent = stripe_payment_intent.create
+    @order.update(payment_intent: payment_intent.id, client_secret: payment_intent.client_secret)
+  end
+
+  def update_payment_intent
+    stripe_payment_intent = StripePaymentintentService.new(@order.stripe_total_price, @user, @order)
+    stripe_payment_intent.update(@order.payment_intent) if stripe_payment_intent.status != 'requires_capture'
+  end
+
+  def capture_payment
+    stripe_payment_intent = StripePaymentintentService.new(nil, @user, @order)
+    stripe_payment_intent.capture
+  end
+end
