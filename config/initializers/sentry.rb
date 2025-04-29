8885404cc66122881619fb42bcc6f2a48bce5668
Leon Skinner <Leon.skinner@bt.com>
2025-04-29 09:03:15 +0100

    add sentry slow code configuration

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,7 +4,11 @@ unless Rails.env.local?
   Sentry.init do |config|
     config.breadcrumbs_logger = [:active_support_logger]
     config.dsn = Rails.application.credentials.dig(:sentry, :DNS)
+    config.traces_sample_rate = 1.0
+    config.profiles_sample_rate = 1.0
     config.enable_tracing = true
     config.send_default_pii = true
   end  
 end
+
+  
